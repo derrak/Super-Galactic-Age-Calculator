ff5b7523749d5a83c44588d24e29b0ae216726ae
Derrak Richard <derrak@gmail.com>
2022-06-24 16:39:00 -0700

    update lefeExpectOutput method in LifeExpect class to pass test

--- src/js/lifeExpect.js
+++ src/js/lifeExpect.js
@@ -10,14 +10,12 @@ export default class LifeExpect{
     this.earthLifeExpect = parseFloat(earthLifeExpectYears.toFixed(2));
   }
 
-  lifeExpectOutput(eAge, eLifeExpt) {
-    let age = eAge;
-    let expect = eLifeExpt;
-    if (age > expect){
-      return true;
+  lifeExpectOutput() {
+    if (this.earthAge > this.earthLifeExpect){
+      return `Congrats! You should be dead! You've surpassed you life expectancy by the eqivilent of ${this.mercury*-1} Mercurian years, ${this.venus*-1} Venusian years, ${this.mars*-1} Martian years, or ${this.jupiter*-1} Jovian years!`;
       }
     else {
-      return false;
+      return `Congrats! You're not dead yet! You've got the eqivilent of ${this.mercury} Mercurian years, ${this.venus} Venusian years, ${this.mars} Martian years, or ${this.jupiter} Jovian years left to live!`;
     }
   }
 
