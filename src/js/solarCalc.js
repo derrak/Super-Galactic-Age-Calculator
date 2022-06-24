469ede611b80d1b03c0f46aea00bc95f23b3021f
Derrak Richard <derrak@gmail.com>
2022-06-24 11:40:23 -0700

    passed second test

--- src/js/solarCalc.js
+++ src/js/solarCalc.js
@@ -1,3 +1,7 @@
 export default function SolarCalc(earthAgeYears){
   this.mercury = earthAgeYears * .24;
+  this.venus = earthAgeYears * .62;
+  this.mars = earthAgeYears * 1.88;
+  this.jupiter = earthAgeYears * 11.86;
 }
+   
