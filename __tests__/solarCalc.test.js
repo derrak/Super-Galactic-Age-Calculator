e0ecd8fb137c57f8bb31b05eafeed2a488e6e791
Derrak Richard <derrak@gmail.com>
2022-06-24 11:34:28 -0700

    second test: 'red phase'

--- __tests__/solarCalc.test.js
+++ __tests__/solarCalc.test.js
@@ -4,6 +4,15 @@ describe('SolarCalc', () => {
   test('should correctly create a solarYearsCalc object with one calc', () => {
     const solarYearsCalc = new SolarCalc(42);
     expect(solarYearsCalc.mercury).toEqual(10.08)
+
+    test('should correctly create a solarYearsCalc object with multiple year calcs', () => {
+      const solarYearsCalc = new SolarCalc(42);
+      expect(solarYearsCalc.mercury).toEqual(10.08)
+      expect(solarYearsCalc.venus).toEqual(26.04)
+      expect(solarYearsCalc.mars).toEqual(84.6)
+      expect(solarYearsCalc.jupiter).toEqual(498.12)
+    });
+
   });
 });
 
