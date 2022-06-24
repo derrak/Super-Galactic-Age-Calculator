bfd6e02836327315823be7baa7223fadf6712797
Derrak Richard <derrak@gmail.com>
2022-06-24 12:30:30 -0700

    passed third test 'Green phase'

--- src/js/solarCalc.js
+++ src/js/solarCalc.js
@@ -1,7 +1,16 @@
-export default function SolarCalc(earthAgeYears){
-  this.mercury = earthAgeYears * .24;
-  this.venus = earthAgeYears * .62;
-  this.mars = earthAgeYears * 1.88;
-  this.jupiter = earthAgeYears * 11.86;
+export default class SolarCalc{
+  constructor(earthAgeYears) {
+    this.mercury = earthAgeYears * .24;
+    this.venus = earthAgeYears * .62;
+    this.mars = earthAgeYears * 1.88;
+    this.jupiter = earthAgeYears * 11.86;
+  }
+  
+  yearOutput() {
+    return "Your age in Mercurian years is: " + this.mercury;
+  }  
+
+
+
 }
    
